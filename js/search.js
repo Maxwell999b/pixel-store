a8ef6a39145b5fcc7cf7beeb686e4a269fd14eaa
Abdulrahman Ahmed <bedo150@gmail.com>
2024-01-13 02:19:34 +0200

    updated contact page,search,form functions

---
+++ js/search.js
@@ -0,0 +1,5 @@
+document.getElementById('toggleButton').addEventListener('click', function() {
+    var searchInput = document.querySelector('.search-click');
+    searchInput.classList.toggle('visually-hidden');
+  });
+
