0fcaa70ecfae9230dc12636d16dd16fc42335d21
Abdulrahman Ahmed <bedo150@gmail.com>
2024-01-15 18:14:31 +0200

    fixed on contact,Home,JS script

--- js/script.js
+++ js/script.js
@@ -1,34 +1,39 @@
-var divContainer = document.getElementById("containeritems")
-fetch('https://mocki.io/v1/7418b292-1c64-4a7c-ab89-3a69b0191ec5', { method: 'get' } )
-.then(response => response.json())
-.then(json => container.innerHTML = JSON.parse(json));
-function getProducts(){
-    fetch('https://mocki.io/v1/7418b292-1c64-4a7c-ab89-3a69b0191ec5', { method: 'get' } )
-    .then(response => response.json())
-    .then(json => 
-        {
-            let str =''
+var divContainer = document.getElementById("containeritems");
+
+function fetchProducts() {
+    return fetch('https://mocki.io/v1/7418b292-1c64-4a7c-ab89-3a69b0191ec5', { method: 'get' })
+        .then(response => response.json());
+}
+
+function getProducts() {
+    fetchProducts()
+        .then(json => {
+            let fragment = document.createDocumentFragment();
+
             for (const item of json) {
-                displayProd(item)
+                displayProd(item, fragment);
             }
+
+            divContainer.appendChild(fragment);
         });
 }
 
-function displayProd(p) {
-    let title = p.name
-    let price = p.price
-    let thumbnail = p.imgSrc
-    var divPrd = document.createElement("div")
-    divPrd.innerHTML += "<br>" + title + "<br>" + price + "<br>" + "<img class='img_item' src='" + thumbnail + "'>"
-    divPrd.setAttribute("class", "item")
-    divContainer.append(divPrd)
-  }
+function displayProd(p, container) {
+    let title = p.name;
+    let price = p.price;
+    let thumbnail = p.imgSrc;
+    var divPrd = document.createElement("div");
+    divPrd.innerHTML = "<br>" + title + "<br>" + price + "<br>" + "<img class='img_item' src='" + thumbnail + "'>";
+    divPrd.classList.add("item");
+    container.appendChild(divPrd);
+}
+
 getProducts();
 
-    // Copy textarea content to the overlaying div
-    var textarea = document.querySelector('.form-control');
-    var textareaContent = document.querySelector('.textarea-content');
+// Other parts of your code remain unchanged
+var textarea = document.querySelector('.form-control');
+var textareaContent = document.querySelector('.textarea-content');
 
-    textarea.addEventListener('input', function() {
-      textareaContent.textContent = textarea.value;
-    });
+textarea.addEventListener('input', function () {
+    textareaContent.textContent = textarea.value;
+});
