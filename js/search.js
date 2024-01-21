2acc26ced13a5ac6afe0de786c22fce97b274092
Abdulrahman Ahmed <bedo150@gmail.com>
2024-01-21 22:51:44 +0200

    Working on search Function

--- js/search.js
+++ js/search.js
@@ -1,5 +1,5 @@
 document.getElementById('toggleButton').addEventListener('click', function() {
-    var searchInput = document.querySelector('.search-click');
-    searchInput.classList.toggle('visually-hidden');
-  });
+  var searchInput = document.querySelector('.search-click');
+  searchInput.classList.toggle('visually-hidden');
+});
 
