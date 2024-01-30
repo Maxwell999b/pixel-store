44aef63121885a718cbdde354170edb4cf95636f
Abdulrahman Ahmed <bedo150@gmail.com>
2024-01-30 08:54:04 +0200

    notifications pop-up sounds applied

--- js/toast.js
+++ js/toast.js
@@ -1,4 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+  // Create an audio element for the notification sound
+  var notificationSound = new Audio('../sounds/notifications-sound-toast-play.mp3');
+
+  // Ignore error: undeclared variable 'bootstrap'
+  // Create a toast instance
   var myToast = new bootstrap.Toast(document.querySelector('.toast'));
 
   // Track whether the toast has been shown
@@ -6,18 +11,29 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Function to check if the user has scrolled to the footer
   function checkScroll() {
-      var footer = document.querySelector('#toastfooter'); 
-      var footerOffset = footer.offsetTop;
-      var scrollTop = window.scrollY;
-      var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+    var footer = document.querySelector('#toastfooter');
+    var footerOffset = footer.offsetTop;
+    var scrollTop = window.scrollY;
+    var windowHeight = window.innerHeight || document.documentElement.clientHeight;
 
-      // Check if the user has scrolled to the footer
-      if (scrollTop + windowHeight >= footerOffset && !toastShown) {
-          // Show the toast
-          myToast.show();
-          toastShown = true;
-      }
+    // Check if the user has scrolled to the footer
+    if (scrollTop + windowHeight >= footerOffset && !toastShown) {
+      // Show the toast
+      myToast.show();
+      toastShown = true;
+    }
   }
+
+  // Function to play the notification sound
+  function playNotificationSound() {
+    // Play the notification sound
+    notificationSound.play();
+  }
+
+  // Add 'shown.bs.toast' event listener to play sound once the toast is shown
+  document.querySelector('.toast').addEventListener('shown.bs.toast', function () {
+    playNotificationSound();
+  });
 
   // Add scroll event listener
   window.addEventListener('scroll', checkScroll);
