a105570c95759a38da8228667b50f311b1cdb14f
Abdulrahman Ahmed <bedo150@gmail.com>
2024-03-23 00:24:16 +0200

    update the API link on site *wasn't working*:3

--- js/products.js
+++ js/products.js
@@ -5,188 +5,188 @@ var searchInput = document.getElementById("searchInput");
 const pageCategory = getPageCategory();
 
 function loadApi() {
-    fetch('https://mocki.io/v1/5c698084-1e13-4abb-be1d-360d47c5a168')
-        .then(response => response.json())
-        .then(data => {
-            if (pageCategory === 'Hoodie') {
-                displayProducts(data.filter(item => item.category === 'Hoodie'));
-            } else if (pageCategory === 'Pants') {
-                displayProducts(data.filter(item => item.category === 'Pants'));
-            } else if (pageCategory === 'Backpack') {
-                displayProducts(data.filter(item => item.category === 'Backpack'));
-            } else if (pageCategory === 'Belts') {
-                displayProducts(data.filter(item => item.category === 'Belts'));
-            } else if (pageCategory === 'Shoes') {
-                displayProducts(data.filter(item => item.category === 'Shoes'));
-            } else if (pageCategory === 'Socks') {
-                displayProducts(data.filter(item => item.category === 'Socks'));
-            } else {
-                displayProducts(data);
-            }
-        });
-}
-
-function getProductsHoodie() {
-    fetch('https://mocki.io/v1/5c698084-1e13-4abb-be1d-360d47c5a168', { method: 'get' })
-        .then(response => response.json())
-        .then(data => {
-            // Display only Hoodie products
-            displayProducts(data.filter(item => item.category === 'Hoodie'));
-        });
-}
-
-function getProductsPants() {
-    fetch('https://mocki.io/v1/5c698084-1e13-4abb-be1d-360d47c5a168', { method: 'get' })
-        .then(response => response.json())
-        .then(data => {
-            // Display only Pants products
-            displayProducts(data.filter(item => item.category === 'Pants'));
-        });
-}
-
-function getProductsBackpack() {
-    fetch('https://mocki.io/v1/5c698084-1e13-4abb-be1d-360d47c5a168', { method: 'get' })
-        .then(response => response.json())
-        .then(data => {
-            // Display only Pants Backpack
-            displayProducts(data.filter(item => item.category === 'Backpack'));
-        });
-}
-
-function getProductsBelts() {
-    fetch('https://mocki.io/v1/5c698084-1e13-4abb-be1d-360d47c5a168', { method: 'get' })
-        .then(response => response.json())
-        .then(data => {
-            // Display only Belts products
-            displayProducts(data.filter(item => item.category === 'Belts'));
-        });
-}
-
-function getProductsShoes() {
-    fetch('https://mocki.io/v1/5c698084-1e13-4abb-be1d-360d47c5a168', { method: 'get' })
-        .then(response => response.json())
-        .then(data => {
-            // Display only Shoes products
-            displayProducts(data.filter(item => item.category === 'Shoes'));
-        });
-}
-
-function getProductsSocks() {
-    fetch('https://mocki.io/v1/5c698084-1e13-4abb-be1d-360d47c5a168', { method: 'get' })
-        .then(response => response.json())
-        .then(data => {
-            // Display only Socks products
-            displayProducts(data.filter(item => item.category === 'Socks'));
-        });
-}
-
-function displayProducts(data) {
-    divContainer.innerHTML = ''; // Clear the container before adding new items
-
-    if (data.length === 0) {
-        // No matching products found
-        divContainer.innerHTML = 'No products found.';
-        return;
-    }
-
-    data.forEach(item => {
-        const { name, price, imgSrc, category } = item;
-        const productElement = createProductElement(name, price, imgSrc, category);
-        divContainer.appendChild(productElement);
+  fetch("https://mocki.io/v1/d2da016c-2ebf-4e5e-9907-3dfeaf86bf82")
+    .then((response) => response.json())
+    .then((data) => {
+      if (pageCategory === "Hoodie") {
+        displayProducts(data.filter((item) => item.category === "Hoodie"));
+      } else if (pageCategory === "Pants") {
+        displayProducts(data.filter((item) => item.category === "Pants"));
+      } else if (pageCategory === "Backpack") {
+        displayProducts(data.filter((item) => item.category === "Backpack"));
+      } else if (pageCategory === "Belts") {
+        displayProducts(data.filter((item) => item.category === "Belts"));
+      } else if (pageCategory === "Shoes") {
+        displayProducts(data.filter((item) => item.category === "Shoes"));
+      } else if (pageCategory === "Socks") {
+        displayProducts(data.filter((item) => item.category === "Socks"));
+      } else {
+        displayProducts(data);
+      }
     });
 }
 
+function getProductsHoodie() {
+  fetch("https://mocki.io/v1/d2da016c-2ebf-4e5e-9907-3dfeaf86bf82", { method: "get" })
+    .then((response) => response.json())
+    .then((data) => {
+      // Display only Hoodie products
+      displayProducts(data.filter((item) => item.category === "Hoodie"));
+    });
+}
+
+function getProductsPants() {
+  fetch("https://mocki.io/v1/d2da016c-2ebf-4e5e-9907-3dfeaf86bf82", { method: "get" })
+    .then((response) => response.json())
+    .then((data) => {
+      // Display only Pants products
+      displayProducts(data.filter((item) => item.category === "Pants"));
+    });
+}
+
+function getProductsBackpack() {
+  fetch("https://mocki.io/v1/d2da016c-2ebf-4e5e-9907-3dfeaf86bf82", { method: "get" })
+    .then((response) => response.json())
+    .then((data) => {
+      // Display only Pants Backpack
+      displayProducts(data.filter((item) => item.category === "Backpack"));
+    });
+}
+
+function getProductsBelts() {
+  fetch("https://mocki.io/v1/d2da016c-2ebf-4e5e-9907-3dfeaf86bf82", { method: "get" })
+    .then((response) => response.json())
+    .then((data) => {
+      // Display only Belts products
+      displayProducts(data.filter((item) => item.category === "Belts"));
+    });
+}
+
+function getProductsShoes() {
+  fetch("https://mocki.io/v1/d2da016c-2ebf-4e5e-9907-3dfeaf86bf82", { method: "get" })
+    .then((response) => response.json())
+    .then((data) => {
+      // Display only Shoes products
+      displayProducts(data.filter((item) => item.category === "Shoes"));
+    });
+}
+
+function getProductsSocks() {
+  fetch("https://mocki.io/v1/d2da016c-2ebf-4e5e-9907-3dfeaf86bf82", { method: "get" })
+    .then((response) => response.json())
+    .then((data) => {
+      // Display only Socks products
+      displayProducts(data.filter((item) => item.category === "Socks"));
+    });
+}
+
+function displayProducts(data) {
+  divContainer.innerHTML = ""; // Clear the container before adding new items
+
+  if (data.length === 0) {
+    // No matching products found
+    divContainer.innerHTML = "No products found.";
+    return;
+  }
+
+  data.forEach((item) => {
+    const { name, price, imgSrc, category } = item;
+    const productElement = createProductElement(name, price, imgSrc, category);
+    divContainer.appendChild(productElement);
+  });
+}
+
 function createProductElement(title, price, thumbnail) {
-    const divPrd = document.createElement("div");
-    divPrd.classList.add("item");
-    divPrd.innerHTML = `
+  const divPrd = document.createElement("div");
+  divPrd.classList.add("item");
+  divPrd.innerHTML = `
         <div class="product-title">${title}</div>
         <div class="product-price">${price}</div>
         <img class='img_item' src='${thumbnail}' loading='lazy'>
         <button class="add-to-cart-button" onclick="addToCart('${title}', '${price}', '${thumbnail}')">Add to Cart</button>
     `;
-    return divPrd;
+  return divPrd;
 }
 function searchProducts(query) {
-    fetch('https://mocki.io/v1/5c698084-1e13-4abb-be1d-360d47c5a168', { method: 'get' })
-        .then(response => response.json())
-        .then(Data => {
-            divContainer.innerHTML = '';
+  fetch("https://mocki.io/v1/d2da016c-2ebf-4e5e-9907-3dfeaf86bf82", { method: "get" })
+    .then((response) => response.json())
+    .then((Data) => {
+      divContainer.innerHTML = "";
 
-            const isProductsPage = window.location.pathname.includes('Products.html');
-            
-            const filteredData = Data.filter(item =>
-                (isProductsPage || item.category === pageCategory) &&
-                (item.name.toLowerCase().includes(query.toLowerCase()) ||
-                    item.price.toString().includes(query))
-            );
+      const isProductsPage = window.location.pathname.includes("Products.html");
 
-            if (filteredData.length === 0) {
-                // No matching products found
-                if (isProductsPage) {
-                    // If it's the Products Page, display "No products found."
-                    divContainer.innerHTML = 'No products found.';
-                    return;
-                }
-            }
-            
-            // If there are matching products, proceed to display them
-            filteredData.forEach(item => {
-                const { name, price, imgSrc, category } = item;
-                const productElement = createProductElement(name, price, imgSrc, category);
-                divContainer.appendChild(productElement);
-            });
-        });
+      const filteredData = Data.filter(
+        (item) =>
+          (isProductsPage || item.category === pageCategory) &&
+          (item.name.toLowerCase().includes(query.toLowerCase()) || item.price.toString().includes(query))
+      );
+
+      if (filteredData.length === 0) {
+        // No matching products found
+        if (isProductsPage) {
+          // If it's the Products Page, display "No products found."
+          divContainer.innerHTML = "No products found.";
+          return;
+        }
+      }
+
+      // If there are matching products, proceed to display them
+      filteredData.forEach((item) => {
+        const { name, price, imgSrc, category } = item;
+        const productElement = createProductElement(name, price, imgSrc, category);
+        divContainer.appendChild(productElement);
+      });
+    });
 }
 
 function debounce(func, delay) {
-    let timeout;
-    return function () {
-        const context = this;
-        const args = arguments;
-        clearTimeout(timeout);
-        timeout = setTimeout(() => func.apply(context, args), delay);
-    };
+  let timeout;
+  return function () {
+    const context = this;
+    const args = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), delay);
+  };
 }
 
 window.onload = function () {
-    if (pageCategory === 'Hoodie') {
-        getProductsHoodie();
-    } else if (pageCategory === 'Pants') {
-        getProductsPants();
-    }else if (pageCategory === 'Backpack') {
-        getProductsBackpack();
-    }else if (pageCategory === 'Belts') {
-        getProductsBelts();
-    }else if (pageCategory === 'Shoes') {
-        getProductsShoes();
-    }else if (pageCategory === 'Socks') {
-        getProductsSocks();
-    } else {
-        loadApi();
-    }
+  if (pageCategory === "Hoodie") {
+    getProductsHoodie();
+  } else if (pageCategory === "Pants") {
+    getProductsPants();
+  } else if (pageCategory === "Backpack") {
+    getProductsBackpack();
+  } else if (pageCategory === "Belts") {
+    getProductsBelts();
+  } else if (pageCategory === "Shoes") {
+    getProductsShoes();
+  } else if (pageCategory === "Socks") {
+    getProductsSocks();
+  } else {
+    loadApi();
+  }
 
-    // Add submit event listener to the search form
-    searchForm.addEventListener('submit', function (event) {
-        event.preventDefault(); // Prevent the default form submission behavior
-        var searchTerm = searchInput.value;
-        searchProducts(searchTerm);
-    });
+  // Add submit event listener to the search form
+  searchForm.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    var searchTerm = searchInput.value;
+    searchProducts(searchTerm);
+  });
 
-    // Add input event listener to the search input
-    searchInput.addEventListener('input', function () {
-        var searchTerm = searchInput.value;
-        debounceSearch(searchTerm);
-    });    
+  // Add input event listener to the search input
+  searchInput.addEventListener("input", function () {
+    var searchTerm = searchInput.value;
+    debounceSearch(searchTerm);
+  });
 };
 
-function getPageCategory() { 
-    // Get the current page URL 
-    const currentPageUrl = window.location.pathname; 
-     
-    // Extract the category from the URL (assuming URLs like "Products.html" and "Hoodie.html") 
-    const categoryMatch = currentPageUrl.match(/\/(\w+)\.html/); 
-     
-    // Return the category or a default value (e.g., 'All' or 'Products') 
-    return categoryMatch ? categoryMatch[1] : '/Products.html'; 
+function getPageCategory() {
+  // Get the current page URL
+  const currentPageUrl = window.location.pathname;
+
+  // Extract the category from the URL (assuming URLs like "Products.html" and "Hoodie.html")
+  const categoryMatch = currentPageUrl.match(/\/(\w+)\.html/);
+
+  // Return the category or a default value (e.g., 'All' or 'Products')
+  return categoryMatch ? categoryMatch[1] : "/Products.html";
 }
--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
 var divContainer = document.getElementById("containeritems");
 
 function fetchProducts() {
-    return fetch('https://mocki.io/v1/5c698084-1e13-4abb-be1d-360d47c5a168', { method: 'get' })
-        .then(response => response.json());
+  return fetch("https://mocki.io/v1/d2da016c-2ebf-4e5e-9907-3dfeaf86bf82", { method: "get" }).then((response) =>
+    response.json()
+  );
 }
 
 function getProducts() {
-    fetchProducts()
-        .then(json => {
-            let fragment = document.createDocumentFragment();
+  fetchProducts().then((json) => {
+    let fragment = document.createDocumentFragment();
 
-            for (const item of json) {
-                displayProd(item, fragment);
-            }
+    for (const item of json) {
+      displayProd(item, fragment);
+    }
 
-            divContainer.appendChild(fragment);
-        });
+    divContainer.appendChild(fragment);
+  });
 }
 
 function displayProd(p, container) {
-    let title = p.name;
-    let price = p.price;
-    let thumbnail = p.imgSrc;
-    var divPrd = document.createElement("div");
-    divPrd.innerHTML = "<br>" + title + "<br>" + price + "<br>" + "<img class='img_item' src='" + thumbnail + "'>";
-    divPrd.classList.add("item");
-    container.appendChild(divPrd);
+  let title = p.name;
+  let price = p.price;
+  let thumbnail = p.imgSrc;
+  var divPrd = document.createElement("div");
+  divPrd.innerHTML = "<br>" + title + "<br>" + price + "<br>" + "<img class='img_item' src='" + thumbnail + "'>";
+  divPrd.classList.add("item");
+  container.appendChild(divPrd);
 }
 
 getProducts();
 
 // Other parts of your code remain unchanged
-var textarea = document.querySelector('.form-control');
-var textareaContent = document.querySelector('.textarea-content');
+var textarea = document.querySelector(".form-control");
+var textareaContent = document.querySelector(".textarea-content");
 
-textarea.addEventListener('input', function () {
-    textareaContent.textContent = textarea.value;
+textarea.addEventListener("input", function () {
+  textareaContent.textContent = textarea.value;
 });
