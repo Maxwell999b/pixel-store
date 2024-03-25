9c27b62abd7283a6ba4722edb724ef5ae6e111b5
Abdulrahman Ahmed <bedo150@gmail.com>
2024-03-25 21:06:13 +0200

    pants #2

--- js/products.js
+++ js/products.js
@@ -82,7 +82,7 @@ function getProductsSocks() {
 
 function displayProducts(data) {
   divContainer.innerHTML = ""; // Clear the container before adding new items
-
+  debugger;
   if (data.length === 0) {
     // No matching products found
     divContainer.innerHTML = "No products found.";
